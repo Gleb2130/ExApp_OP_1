8a97b56696b0ca9546497ec0d78f8717ddc11105
Gleb <114813737+Gleb2130@users.noreply.github.com>
2024-11-04 13:29:48 +0100

    Add files via upload

---
+++ arxiv_bibdata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for id in "$@"; do
+	url="https://arxiv.org/abs/$id"
+	md=$(curl -s "$url" | grep -Po '<meta name="citation_\w+" content="[^"]+"')
+	echo "--title=\"$(echo "$md" | grep -Po 'citation_title" content="\K[^"]+')\""
+	echo "author=\"$(echo "$md" | grep -Po 'citation_author" content="\K[^"]+' | tr '\n' ' ')\""
+	echo "date=\"$(echo "$md" | grep -Po 'citation_date" content="\K[^"]+')\""
+	echo "online_date=\"$(echo "$md" | grep -Po 'citation_online_date" content="\K[^"]+' | head -n1)\""
+	echo "pdf_url=\"https://arxiv.org/pdf/$id\""
+	echo "arxiv_id=\"$id\""
+	echo
+done
+
