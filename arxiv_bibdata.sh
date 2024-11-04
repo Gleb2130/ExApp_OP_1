d20fb076521f72ddf0e66725c799fdc4398b45f3
Gleb <114813737+Gleb2130@users.noreply.github.com>
2024-11-04 13:33:55 +0100

    Update arxiv_bibdata.sh

--- arxiv_bibdata.sh
+++ arxiv_bibdata.sh
@@ -2,7 +2,7 @@
 
 for id in "$@"; do
 	url="https://arxiv.org/abs/$id"
-	md=$(curl -s "$url" | grep -Po '<meta name="citation_\w+" content="[^"]+"')
+	md=$(curl -s -L -A Lynx "$url" | grep -Po '<meta name="citation_\w+" content="[^"]+"')
 	echo "--title=\"$(echo "$md" | grep -Po 'citation_title" content="\K[^"]+')\""
 	echo "author=\"$(echo "$md" | grep -Po 'citation_author" content="\K[^"]+' | tr '\n' ' ')\""
 	echo "date=\"$(echo "$md" | grep -Po 'citation_date" content="\K[^"]+')\""
